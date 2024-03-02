a85442682fc519568277f85e0a0f58e2ef8374b0
Mark Jagyo <mark.jagyo@gmail.com>
2024-03-02 12:35:14 +0800

    Create anotherFunction.groovy

---
+++ vars/anotherFunction.groovy
@@ -0,0 +1,17 @@
+/**
+    Arguments:
+        test - this is the definition
+        anotherArgs - another definition for different args
+        name - the definition of name that has a default of "Alice"
+    
+    Usage:
+        anotherFunction("valueForTest", "valueForAnotherArgs", "valueForNameArgs")
+*/
+
+def call(String test, String anotherArgs, String name = "Alice") {
+    script {
+        sh """
+            echo Hi ${name}
+        """
+    }
+}
